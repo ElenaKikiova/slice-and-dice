c05c34c2bafadbfb7756748f8879e4dc124515b4
Elena Kikiova <elenakikiova@mail.ru>
2021-06-22 22:24:46 +0300

    Almost there

---
+++ script.js
@@ -0,0 +1,92 @@
+let collapseButton = document.getElementsByName("collapseButton");
+
+let works = document.getElementsByClassName("work");
+
+let slide = document.getElementById("slide");
+let quotePhoto = document.getElementById("quotePhoto");
+let quoteText = document.getElementById("quoteText");
+let author = document.getElementById("author");
+let activeSlide = 1;
+
+// Data for the carousel
+
+let slides = [
+
+  {
+    "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laboriosam impedit vero Possimus?",
+    author: "SUSAN SIMS, INTERACTION DESIGNER AT XYZ "
+  },
+  {
+    "quote": "ipsum dolor sit amet consectetur adipisicing elit. Sequi laboriosam impedit vero",
+    "author": "BEVERLY LITTLE"
+  }, 
+  {
+    "quote": "Commodi dolorum veniam quas iure saepe,Commodi dolorum veniam quas iure saepe",
+    "author": "TIMOTHEE REED "
+  }, 
+  {
+    "quote": "Sequi laboriosam impedit vero Possimus?Sequi laboriosam impedit vero Possimus?",
+    "author": "SOME SMART GUY"
+  },
+  {
+    "quote": "Aliquid modi blanditiis quasi provident. Commodi dolorum veniam quas iure saepe",
+    "author": "VICTORIA VALDEZ"
+  }
+
+]
+
+let quotes = [
+  ,
+]
+
+let authors = {
+
+}
+
+for(let i = 0; i < works.length; i++){
+  works[i].onmouseover = () => {showCaption(works[i])};
+  works[i].onmouseleave = () => {hideCaption(works[i])};
+}
+
+function showCaption(work){
+  let backdrop = work.getElementsByClassName("backdrop")[0];
+  let caption = work.getElementsByClassName("caption")[0];
+  backdrop.style.opacity = 1;
+  caption.style.opacity = 1;
+}
+
+function hideCaption(work){
+  let backdrop = work.getElementsByClassName("backdrop")[0];
+  let caption = work.getElementsByClassName("caption")[0];
+  backdrop.style.opacity = 0;
+  caption.style.opacity = 0;
+}
+
+let carousel_dots = document.getElementsByClassName("dot");
+
+for(let i = 0; i < carousel_dots.length; i++){
+  carousel_dots[i].onclick = () => {changeSlide(carousel_dots[i].getAttribute("data-id"))};
+}
+
+function changeSlide(id){
+  console.log(id);
+  if(activeSlide != id){
+    slide.style.opacity = 0;
+    activeSlide = id;
+    slideInfo = slides[id - 1];
+
+    console.log(slideInfo.author, author.innerHTML)
+
+    setTimeout(() => {
+
+      quotePhoto.src = "images/carousel-" + id + ".jpg";
+      quoteText.innerHtml = slideInfo.quote;
+      author.innerHtml = slideInfo.author;
+      document.getElementById("active").id = null;
+      document.querySelector(".dot[data-id='" + id + "'").id = "active";
+
+      slide.style.opacity = 1;
+
+    }, 300);
+  }
+}
